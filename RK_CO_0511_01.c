5c1adeec88ffe298eb9953ea8d6335b5b6c7dfcb
常盤　美禄 <mtkdns7@gmail.com>
2023-05-23 15:13:57 +0900

    Fix: 5/23課題に変更

--- RK_CO_0511_01.c
+++ RK_CO_0511_01.c
@@ -4,10 +4,25 @@
 //
 #define PI 3.141592653589793
 #define PI2 2.0 * 3.141592653589793
-#define NN 4
-#define KK 1.0
+#define ALPHA 1.0
+#define BETA 1.0
+#define VPL 0.1
+#define FRICMAX 1.0
+#define KKZ 1.0
+//NEVER MOVE NEGATIVE DIRECTION
+#define BK1DFOREL(xx0,xx1,xx2,xxp)  (  (xx1 + xx2 - 2.0*xx0) + KKZ*(xxp-xx0) )
+#define FLAG00(xxx)   ( (int)( xxx*1.0E-10 + 9.99999999999999E-1 ) ) // FLAG of xxx >0 (->1) or <=0 (->0)
+#define FLAGABTH(xxx,xth)   ( (int)( (fabs(xxx)-xth)*1.0E-10 + 9.99999999999999E-1 ) ) // FLAG of abs(xxx) - xth  > 0 (->1) or <=0 (->0)
+#define FLAGAB00(xxx)       ( (int)(  fabs(xxx)     *1.0E-10 + 9.99999999999999E-1 ) ) // FLAG of abs(xxx)   > 0 (->1) or <=0 (->0)
+#define FLAGSLIP(forcel,maxstfric,vvv)  ( 1- ( 1-FLAGABTH(forcel,maxstfric) )*( 1-FLAGAB00(vvv) ) ) // FLAG of ( ( abs(forcel) - maxstfric  > 0 ) OR (vvv > 0) ) (->1)
+#define BK1DFORKF(vvv)  ( - ( FLAG00(vvv)-FLAG00(-vvv) ) / ( 1.0+ALPHA*fabs(vvv) ) ) // Kinetic Friction Force
+#define ReLU(vvv)  ( 0.5*( vvv + fabs(vvv) ) )
+#define FFVV(forcel,maxstfric,vvv)    ( FLAGSLIP(forcel,maxstfric,vvv)*( forcel + BETA*BK1DFORKF(vvv) ) )
+//
+#define NN 2
+//#define KK 1.0
 #define OMEGA 1.0
-#define FILENAME "dat0427-06.dat"
+#define FILENAME "dat0522-01.dat"
 
 //
 void rk4(double, double, double *, double *, double *, double *);
@@ -24,17 +39,13 @@ void main()
   fpcur = fopen(FILENAME, "w");
 
   /* initial condition*/
-  xx[1] = 0.0;
-  xx[2] = 1.0;
-  xx[3] = 2.0;
-  xx[4] = 3.0;
-  yy[1] = 0.0;
-  yy[2] = 0.0;
-  yy[3] = 0.0;
-  yy[4] = 0.0;
+  xx[1] = -1.0;
+  xx[2] =  1.0;
+  yy[1] =  0.0;
+  yy[2] =  0.0;
  /* initial condition*/
  
-  dt = 0.01*PI2/OMEGA;
+  dt = 0.01;
   imaxtime=maxtime/dt;
 
   for (int itime = 0; itime <= imaxtime; itime++)
@@ -50,8 +61,8 @@ void main()
       yy[ii] = yyout[ii];
     }
 
-    printf("time = %10.3lf xx1= %10.3lf   xx2=  %10.3lf xx3=  %10.3lf xx4=  %10.3lf\n", time, xx[1], xx[2],xx[3],xx[4]);
-    fprintf(fpcur, "%10.3lf %10.3lf %10.3lf %10.3lf %10.3lf %10.3lf %10.3lf %10.3lf %10.3lf\n", time, xx[1],xx[2],xx[3],xx[4],yy[1],yy[2],yy[3],yy[4]);
+    printf("time = %10.3lf xx1= %10.3lf   xx2=  %10.3lf xx3=  %10.3lf xx4=  %10.3lf\n", time, xx[1], xx[2]);
+    fprintf(fpcur, "%10.3lf %10.3lf %10.3lf %10.3lf %10.3lf %10.3lf %10.3lf %10.3lf %10.3lf\n", time, xx[1],xx[2],yy[1],yy[2]);
   }
 
   fclose(fpcur);
@@ -72,7 +83,7 @@ void rk4(double dt, double time, double *xx, double *yy, double *xxout, double *
   for (int ii = 1; ii <= NN; ii++)
   {
     xxtt[ii] = xx[ii] + dthh * ddxx1[ii];
-    yytt[ii] = yy[ii] + dthh * ddyy1[ii];
+    yytt[ii] = ReLU( yy[ii] + dthh * ddyy1[ii] );
   }
   //
   xxtt[0]      = xxtt[1];
@@ -85,7 +96,7 @@ void rk4(double dt, double time, double *xx, double *yy, double *xxout, double *
   for (int ii = 1; ii <= NN; ii++)
   {
     xxtt[ii] = xx[ii] + dthh * ddxx2[ii];
-    yytt[ii] = yy[ii] + dthh * ddyy2[ii];
+    yytt[ii] = ReLU( yy[ii] + dthh * ddyy2[ii] );
   }
   //
   xxtt[0]      = xxtt[1];
@@ -98,7 +109,7 @@ void rk4(double dt, double time, double *xx, double *yy, double *xxout, double *
   for (int ii = 1; ii <= NN; ii++)
   {
     xxtt[ii] = xx[ii] + dt * ddxx3[ii];
-    yytt[ii] = yy[ii] + dt * ddyy3[ii];
+    yytt[ii] = ReLU( yy[ii] + dt * ddyy3[ii] );
   }
   //
   xxtt[0]      = xxtt[1];
@@ -111,17 +122,25 @@ void rk4(double dt, double time, double *xx, double *yy, double *xxout, double *
   for (int ii = 1; ii <= NN; ii++)
   {
     xxout[ii] = xx[ii] + dt6 * (ddxx1[ii] + 2.0 * ddxx2[ii] + 2.0 * ddxx3[ii] + ddxx4[ii]);
-    yyout[ii] = yy[ii] + dt6 * (ddyy1[ii] + 2.0 * ddyy2[ii] + 2.0 * ddyy3[ii] + ddyy4[ii]);
+    yyout[ii] = ReLU( yy[ii] + dt6 * (ddyy1[ii] + 2.0 * ddyy2[ii] + 2.0 * ddyy3[ii] + ddyy4[ii]) );
   }
   /* end of 4-thd step*/
 }
 //
 void derivxx(double time, double *xx, double *yy, double *ddxx)
 {
-  for (int ii = 1; ii <= NN; ii++)    ddxx[ii] = yy[ii];
+  for (int ii = 1; ii <= NN; ii++)    ddxx[ii] = ReLU( yy[ii] );
 }
 //
 void derivyy(double time, double *xx, double *yy, double *ddyy)
 {
-  for (int ii = 1; ii <= NN; ii++)    ddyy[ii] = xx[ii + 1] + xx[ii - 1] - 2.0 * xx[ii] + KK * (time-xx[ii]);
+ double forceel,xpph;
+
+ xpph=VPL*time;
+
+ for (int ii = 1; ii <= NN; ii++)
+ {
+  forceel = BK1DFOREL(xx[ii],xx[ii-1],xx[ii+1],xpph);
+  ddyy[ii]= FFVV(forceel,FRICMAX,yy[ii]);
+ }
 }
